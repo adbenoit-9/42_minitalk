ae371a60492bf621b5117c559e5d1aafdf11dc99
adbenoit-9 <adeline.benoit@hotmail.com>
2021-10-09 19:43:41 +0200

    norm

--- client.c
+++ client.c
@@ -5,67 +5,67 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/10 14:09:43 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/10/09 18:50:47 by adbenoit         ###   ########.fr       */
+/*   Created: 2021/10/09 19:40:44 by adbenoit          #+#    #+#             */
+/*   Updated: 2021/10/09 19:42:00 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void    ft_error(char *str)
+static void	ft_error(char *str)
 {
-    ft_putstr_fd("Error: ", 2);
-    ft_putendl_fd(str, 2);
-    exit(0);
+	ft_putstr_fd("Error: ", 2);
+	ft_putendl_fd(str, 2);
+	exit(0);
 }
 
-static void    send_char(pid_t pid, char c)
+static void	send_char(pid_t pid, char c)
 {
-    bool    bit;
-    int     i;
+	bool	bit;
+	int		i;
 
-    i = 0;
-    while (i < 8)
-    {
-        bit = (c >> i) & 1;
-        if (bit == 0)
-        {
-            if (kill(pid, SIGUSR1) == -1)
-                ft_error("No such process");
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) == -1)
-                ft_error("No such process");
-        }
-        ++i;
-        usleep(100);
-    }
+	i = 0;
+	while (i < 8)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				ft_error("No such process");
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				ft_error("No such process");
+		}
+		++i;
+		usleep(100);
+	}
 }
 
-static void    send_str(pid_t pid, char* str)
+static void	send_str(pid_t pid, char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    usleep(100);
-    while (str[i])
-    {
-        send_char(pid, str[i]);
-        ++i;
-    }
-    ft_putendl_fd("The string has been sent.", 1);
+	i = 0;
+	usleep(100);
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		++i;
+	}
+	ft_putendl_fd("The string has been sent.", 1);
 }
 
-int             main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    pid_t pid;
-    
-    if (ac != 3)
-        ft_error("Usage: ./client [PID] [string to send]");
-    pid = ft_atoi(av[1]);
-    if (pid < 1)
-        ft_error("Wrong PID");
-    send_str(pid, av[2]);
-    return (0);
+	pid_t	pid;
+
+	if (ac != 3)
+		ft_error("Usage: ./client [PID] [string to send]");
+	pid = ft_atoi(av[1]);
+	if (pid < 1)
+		ft_error("Wrong PID");
+	send_str(pid, av[2]);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,39 +6,37 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 14:09:58 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/10/09 18:54:23 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/10/09 19:43:05 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static void    receive_str(int signum)
+static void	receive_str(int signum)
 {
-    static int  i_bit = 0;
-    static char byte = 0;
+	static int	i_bit = 0;
+	static char	byte = 0;
 
-    if (signum == SIGUSR2)
-        byte += 1 << i_bit;
-    ++i_bit;
-    if (i_bit >= 8)
-    {
-        if (byte)
-            ft_putchar_fd(byte, 1);
-        byte = 0;
-        i_bit = 0;
-    }
+	if (signum == SIGUSR2)
+		byte += 1 << i_bit;
+	++i_bit;
+	if (i_bit >= 8)
+	{
+		if (byte)
+			ft_putchar_fd(byte, 1);
+		byte = 0;
+		i_bit = 0;
+	}
 }
 
-int         main()
+int	main(void)
 {
-    pid_t pid;
-    pid = getpid();
-    ft_putstr_fd("PID: ", 1);
-    ft_putnbr_fd(pid, 1);
-    ft_putchar_fd('\n', 1);
-    signal(SIGUSR1, receive_str);
-    signal(SIGUSR2, receive_str);
-    while (1)
-        pause();
-    return (0);
+	ft_putstr_fd("PID: ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	signal(SIGUSR1, receive_str);
+	signal(SIGUSR2, receive_str);
+	while (1)
+		pause();
+	return (0);
 }
